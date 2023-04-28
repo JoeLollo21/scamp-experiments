18bd652e02882cc60742fa6a435fcc2af747fbc4
Joe Lollo <53008442+JoeLollo21@users.noreply.github.com>
2023-04-27 17:03:39 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ s.start_transcribing()
 melody = [ # This is really just a C chromatic scale
   (60, 0.25), # C, quarter note
   (64, 0.25), # E, quarter note
-  (67, 0.5) # G, half note
-  (72, 0.25) # C, quarter note
-  (76, 1.0) # E, whole note
-  (79, 0.25) # G, quarter note
+  (67, 0.5), # G, half note
+  (72, 0.25), # C, quarter note
+  (76, 1.0), # E, whole note
+  (79, 0.25), # G, quarter note
   (84, 0.25), # C, quarter note
   (None, 0.25), # Quarter rest
   (81, 0.5), # A, half note
